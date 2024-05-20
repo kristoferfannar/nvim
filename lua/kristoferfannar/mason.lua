7cf0d65a1234a60c554953b97768c7f8674ef4da
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-05-20 02:26:54 +0000

    Restructure LSP imports through mason-lspconfig

--- lua/kristoferfannar/mason.lua
+++ lua/kristoferfannar/mason.lua
@@ -1,4 +1,17 @@
+local servers = {
+	"lua_ls",
+	"clangd",
+	"gopls",
+	"tsserver"
+}
+
+
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "gopls" },
+	ensure_installed = servers,
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+	},
 })

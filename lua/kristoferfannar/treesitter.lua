ceec6bb037bed7b4a729bebc4b87e25319a98819
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-07-26 22:49:59 +0000

    Add vimtex 👑

--- lua/kristoferfannar/treesitter.lua
+++ lua/kristoferfannar/treesitter.lua
@@ -22,6 +22,10 @@ require("nvim-treesitter.configs").setup({
 		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
 		-- the name of the parser)
 		disable = function(lang, buf)
+			if lang == "latex" then
+				return true
+			end
+
 			local max_filesize = 100 * 1024 -- 100 KB
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then

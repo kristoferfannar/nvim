a586fdd2245f6549309d47c62719ab56c4218216
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-09-12 00:27:17 -0400

    move whichkey mappings to new API

--- lua/kristoferfannar/whichkey.lua
+++ lua/kristoferfannar/whichkey.lua
@@ -3,70 +3,211 @@ vim.api.nvim_set_keymap("n", "<space>", "", { noremap = true, silent = true })
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 
+local wk = require("which-key")
+
 local function create_new_tab()
 	vim.cmd("tablast")
 	vim.cmd("tabnew")
 	vim.cmd("Oil")
 end
 
-local wk = require("which-key")
+wk.add({
+	{ "<c-h>", "<c-w>h", desc = "Move cursor left", remap = false },
+	{ "<c-j>", "<c-w>j", desc = "Move cursor down", remap = false },
+	{ "<c-k>", "<c-w>k", desc = "Move cursor up", remap = false },
+	{ "<c-l>", "<c-w>l", desc = "Move cursor right", remap = false },
+})
 
-wk.register({
-	["<c-h>"] = { "<c-w>h", "Move cursor left" },
-	["<c-j>"] = { "<c-w>j", "Move cursor down" },
-	["<c-k>"] = { "<c-w>k", "Move cursor up" },
-	["<c-l>"] = { "<c-w>l", "Move cursor right" },
-}, { noremap = true, silent = true, mode = "n" })
-
-wk.register({
-	w = { "<cmd>w<cr>", "Write" },
-	q = { "<cmd>q!<cr>", "Exit file" },
-	x = { "<cmd>x!<cr>", "Write Quit" },
-	X = { "<cmd>xall<cr>", "Write Quit All" },
-	f = {
-		name = "+files",
-		f = { "<cmd>Telescope find_files<cr>", "Local files" },
-		e = { ":Oil --float<cr>", "File explorer" },
-		g = { "<cmd>Telescope live_grep<cr>", "live grep" },
+wk.add({
+	{ "<leader><Tab>", group = "tabs", remap = false },
+	{
+		"<leader><Tab>0",
+		"<cmd>tabfirst<cr>",
+		desc = "goto first tab",
+		remap = false,
 	},
-
-	l = {
-		name = "lsp",
-		f = { "<cmd>lua vim.lsp.buf.format { async = true}<cr>", "Format file" },
-		r = { "<cmd>lua vim.lsp.buf.rename { async = true}<cr>", "Rename word" },
+	{
+		"<leader><Tab>1",
+		"<cmd>tabn 1<cr>",
+		desc = "goto tab 1",
+		remap = false,
 	},
-
-	g = {
-		name = "git",
-		s = { "<cmd>Git<cr><c-w>H<cmd>vertical resize -40<cr>", "Git status" },
-		a = { "<cmd>Git add .<cr>", "Git add ." },
-		c = { "<cmd>Git commit<cr>", "Git commit" },
-		ps = { "<cmd>Git push<cr>", "Git push" },
-		pl = { "<cmd>Git pull<cr>", "Git pull" },
-		l = { "<cmd>Git log<cr><c-w>H<cmd>vertical resize -30<cr>", "Git log" },
-		f = { "<cmd>Git fetch<cr>", "Git fetch" },
-		b = { "<cmd>Twiggy<cr>", "Git branch" },
-		t = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Toggle blame" },
+	{
+		"<leader><Tab>2",
+		"<cmd>tabn 2<cr>",
+		desc = "goto tab 2",
+		remap = false,
 	},
-
-	b = {
-		name = "screen split",
-		l = { ":vsplit<cr><c-w>l", "split right" },
+	{
+		"<leader><Tab>3",
+		"<cmd>tabn 3<cr>",
+		desc = "goto tab 3",
+		remap = false,
 	},
-
-	d = { "<cmd>TroubleToggle<cr>", "Toggle Diagnostics" },
-	["<Tab>"] = {
-		name = "tabs",
-		["<Tab>"] = { create_new_tab, "new tab" },
-		n = { "<cmd>tabnext<cr>", "next tab" },
-		p = { "<cmd>tabprevious<cr>", "previous tab" },
-		["1"] = { "<cmd>tabn 1<cr>", "goto tab 1" },
-		["2"] = { "<cmd>tabn 2<cr>", "goto tab 2" },
-		["3"] = { "<cmd>tabn 3<cr>", "goto tab 3" },
-		["4"] = { "<cmd>tabn 4<cr>", "goto tab 4" },
-		["5"] = { "<cmd>tabn 5<cr>", "goto tab 5" },
-		["6"] = { "<cmd>tabn 6<cr>", "goto tab 6" },
-		["7"] = { "<cmd>tabn 7<cr>", "goto tab 7" },
-		["0"] = { "<cmd>tabfirst<cr>", "goto first tab" },
+	{
+		"<leader><Tab>4",
+		"<cmd>tabn 4<cr>",
+		desc = "goto tab 4",
+		remap = false,
 	},
-}, { noremap = true, silent = true, prefix = "<leader>" })
+	{
+		"<leader><Tab>5",
+		"<cmd>tabn 5<cr>",
+		desc = "goto tab 5",
+		remap = false,
+	},
+	{
+		"<leader><Tab>6",
+		"<cmd>tabn 6<cr>",
+		desc = "goto tab 6",
+		remap = false,
+	},
+	{
+		"<leader><Tab>7",
+		"<cmd>tabn 7<cr>",
+		desc = "goto tab 7",
+		remap = false,
+	},
+	{
+		"<leader><Tab><Tab>",
+		create_new_tab,
+		desc = "new tab",
+		remap = false,
+	},
+	{
+		"<leader><Tab>n",
+		"<cmd>tabnext<cr>",
+		desc = "next tab",
+		remap = false,
+	},
+	{
+		"<leader><Tab>p",
+		"<cmd>tabprevious<cr>",
+		desc = "previous tab",
+		remap = false,
+	},
+	{
+		"<leader>X",
+		"<cmd>xall<cr>",
+		desc = "Write Quit All",
+		remap = false,
+	},
+	{ "<leader>b", group = "screen split", remap = false },
+	{
+		"<leader>bl",
+		":vsplit<cr><c-w>l",
+		desc = "split right",
+		remap = false,
+	},
+	{
+		"<leader>d",
+		"<cmd>TroubleToggle<cr>",
+		desc = "Toggle Diagnostics",
+		remap = false,
+	},
+	{ "<leader>f", group = "files", remap = false },
+	{
+		"<leader>fe",
+		":Oil --float<cr>",
+		desc = "File explorer",
+		remap = false,
+	},
+	{
+		"<leader>ff",
+		"<cmd>Telescope find_files<cr>",
+		desc = "Local files",
+		remap = false,
+	},
+	{
+		"<leader>fg",
+		"<cmd>Telescope live_grep<cr>",
+		desc = "live grep",
+		remap = false,
+	},
+	{ "<leader>g", group = "git", remap = false },
+	{
+		"<leader>ga",
+		"<cmd>Git add .<cr>",
+		desc = "Git add .",
+		remap = false,
+	},
+	{
+		"<leader>gb",
+		"<cmd>Twiggy<cr>",
+		desc = "Git branch",
+		remap = false,
+	},
+	{
+		"<leader>gc",
+		"<cmd>Git commit<cr>",
+		desc = "Git commit",
+		remap = false,
+	},
+	{
+		"<leader>gf",
+		"<cmd>Git fetch<cr>",
+		desc = "Git fetch",
+		remap = false,
+	},
+	{
+		"<leader>gl",
+		"<cmd>Git log<cr><c-w>H<cmd>vertical resize -30<cr>",
+		desc = "Git log",
+		remap = false,
+	},
+	{
+		"<leader>gpl",
+		"<cmd>Git pull<cr>",
+		desc = "Git pull",
+		remap = false,
+	},
+	{
+		"<leader>gps",
+		"<cmd>Git push<cr>",
+		desc = "Git push",
+		remap = false,
+	},
+	{
+		"<leader>gs",
+		"<cmd>Git<cr><c-w>H<cmd>vertical resize -40<cr>",
+		desc = "Git status",
+		remap = false,
+	},
+	{
+		"<leader>gt",
+		"<cmd>Gitsigns toggle_current_line_blame<cr>",
+		desc = "Toggle blame",
+		remap = false,
+	},
+	{ "<leader>l", group = "lsp", remap = false },
+	{
+		"<leader>lf",
+		"<cmd>lua vim.lsp.buf.format { async = true}<cr>",
+		desc = "Format file",
+		remap = false,
+	},
+	{
+		"<leader>lr",
+		"<cmd>lua vim.lsp.buf.rename { async = true}<cr>",
+		desc = "Rename word",
+		remap = false,
+	},
+	{
+		"<leader>q",
+		"<cmd>q!<cr>",
+		desc = "Exit file",
+		remap = false,
+	},
+	{
+		"<leader>w",
+		"<cmd>w<cr>",
+		desc = "Write",
+		remap = false,
+	},
+	{
+		"<leader>x",
+		"<cmd>x!<cr>",
+		desc = "Write Quit",
+		remap = false,
+	},
+})

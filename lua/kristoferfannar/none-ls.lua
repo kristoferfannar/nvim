939a25c0a8ffef45e1ea36cd41c5978f6777ed86
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-04-20 12:42:33 -0400

    change formatting rules

--- lua/kristoferfannar/none-ls.lua
+++ lua/kristoferfannar/none-ls.lua
@@ -2,12 +2,25 @@ local none_ls = require("null-ls")
 
 none_ls.setup({
 	sources = {
-		none_ls.builtins.formatting.biome, -- typescript etc
+		none_ls.builtins.formatting.biome.with({
+			-- organize imports with formatting
+			-- args = { "check", "--organize-imports-enabled=true", "--stdin-file-path", "$FILENAME", "--write" },
+			args = { "format", "--stdin-file-path", "$FILENAME" },
+		}),
+		-- none_ls.builtins.formatting.biome,
 		none_ls.builtins.formatting.stylua, -- lua
 		none_ls.builtins.formatting.prettier.with({
-			filetypes = { "markdown" }, -- markdown
+			filetypes = { "markdown", "html" },
 		}),
 		none_ls.builtins.formatting.black, -- python
+
+		-- format ubpf repo with clang-format-11
+		none_ls.builtins.formatting.clang_format.with({
+			condition = function()
+				return string.find(vim.fn.getcwd(), "ubpf")
+			end,
+			command = "clang-format-11",
+		}),
 	},
 })
 
@@ -22,4 +35,4 @@ local function format_on_save()
 	})
 end
 
-format_on_save()
+-- format_on_save()
